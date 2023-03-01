fdcf2e38b80106a1ba051b114710228f52672382
JaviGF8 <takerprods@gmail.com>
2023-03-01 12:39:30 +0100

    update profile

--- src/components/MainCube/Profile/Profile.jsx
+++ src/components/MainCube/Profile/Profile.jsx
@@ -6,17 +6,17 @@ import Bar from '../../Bar';
 const Profile = ({ ...rest }) => (
   <Section {...rest} title={translate('profile.title')} className="profile-section">
     <p>{translate('profile.header')}</p>
-    <Bar text="React" amount={80} />
-    <Bar text="React Native" amount={50} />
-    <Bar text="HTML" amount={75} />
+    <Bar text="React" amount={85} />
+    <Bar text="React Native" amount={60} />
+    <Bar text="HTML" amount={85} />
     <Bar text="CSS" amount={85} />
+    <Bar text="Jest" amount={65} />
+    <Bar text="NextJs" amount={50} />
     <Bar text="GraphQL" amount={55} />
     <Bar text="Node" amount={65} />
-    <Bar text="Mongo" amount={70} />
-    <Bar text="Angular" amount={40} />
-    <Bar text="Java" amount={60} />
-    <Bar text="Springboot" amount={20} />
-    <Bar text="Vue" amount={20} />
+    <Bar text="Mongo" amount={60} />
+    <Bar text="Angular" amount={30} />
+    <Bar text="Java" amount={40} />
     <p>{translate('profile.text')}</p>
     <p>{translate('profile.text2')}</p>
   </Section>
